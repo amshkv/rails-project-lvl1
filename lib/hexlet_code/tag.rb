6752b478935ceafc03c1545d72ab5737fc784365
Andrey Moshkov <fakesupp@gmail.com>
2022-10-08 23:30:42 +0300

    :sparkles: add tag module

---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module HexletCode
+  module Tag
+    ELEMENTS_WITHOUT_END_TAG = %w[img br hr input].freeze
+
+    def self.build(tag, **options)
+      attributes = options.map { |key, value| " #{key}=\"#{value}\"" }.join
+      if ELEMENTS_WITHOUT_END_TAG.include?(tag)
+        "<#{tag}#{attributes}>"
+      else
+        "<#{tag}#{attributes}>#{yield if block_given?}</#{tag}>"
+      end
+    end
+  end
+end
